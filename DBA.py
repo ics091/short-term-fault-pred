aa1de63c62b51f178f0428079e1806c0787cf907
ics091 <icsrighttrack@outlook.com>
2020-07-30 11:03:53 +0800

    add weighted DBA

--- DBA.py
+++ DBA.py
@@ -20,11 +20,20 @@ def Medoid(D):
     return medoid
 
 
+def Wtd_medoid(D, W):
+    minSS = float("inf")
+    for S1 in D:
+        tmpSS = 0
+        for S2, w in zip(D, W):
+            tmpSS += w * ((dtw(S1, S2, dist=l2_norm)[0])**2)
+        if tmpSS < minSS:
+            medoid = S1
+            minSS = tmpSS
+    return medoid
+
+
 def DTW_multiple_alignment(Sref, S):
     dist, cost_matrix, cost, path = dtw(Sref, S, dist=l2_norm)
-    # print(cost_matrix)
-    # print(cost)
-    # print(path)
     L = len(Sref)
     alignment = []
     for l in range(L):
@@ -32,18 +41,17 @@ def DTW_multiple_alignment(Sref, S):
     i = cost.shape[0] - 1
     j = cost.shape[1] - 1
     while (i >= 0) and (j >= 0):
-        # print(S[j])
         alignment[i].append(S[j])
         if i == 0:
             j = j - 1
         elif j == 0:
             i = i - 1
         else:
-            score = min(cost[i-1][j-1], cost[i][j-1], cost[i-1][j])
-            if score == cost[i-1][j-1]:
+            score = min(cost[i - 1][j - 1], cost[i][j - 1], cost[i - 1][j])
+            if score == cost[i - 1][j - 1]:
                 i = i - 1
                 j = j - 1
-            elif score == cost[i-1][j]:
+            elif score == cost[i - 1][j]:
                 i = i - 1
             else:
                 j = j - 1
@@ -63,11 +71,33 @@ def DBA_update(Tinit, D):
             alignment[i].append(alignment_for_S[i])
     # average
     for j in range(L):
-        # print(alignment[j])
         for A in alignment[j]:
             for a in A:
                 T[j].append(a)
         T[j] = mean(T[j])
+    return T
+
+
+def Wtd_DBA_update(Tinit, D, W):
+    T = []
+    alignment = []
+    sumWeights = []
+    L = len(Tinit)
+    for l in range(L):
+        alignment.append([])
+        T.append([])
+        sumWeights.append(0)
+    for S, w in zip(D, W):
+        alignment_for_S = DTW_multiple_alignment(Tinit, S)
+        for i in range(L):
+            alignment[i].append([s * w for s in alignment_for_S[i]])
+            sumWeights[i] += w
+    # average
+    for j in range(L):
+        for A in alignment[j]:
+            for a in A:
+                T[j].append(a)
+        T[j] = float(sum(T[j])) / sumWeights[j]
     return T
 
 
@@ -80,16 +110,33 @@ def DBA(D, I):
     return T
 
 
+def Wtd_DBA(D, W, I):
+    # I means times of update
+    T = Wtd_medoid(D, W)
+    for i in range(I):
+        T = Wtd_DBA_update(T, D, W)
+        print(T)
+    return (T)
+
+
 if __name__ == '__main__':
     D = [
-        np.array([1, 3, 6, 6, 5, 3, 2, 0]),
-        np.array([0, 2, 5, 4, 2, 1]),
+        np.array([1, 3, 6, 6, 5, 3, 2]),
+        np.array([0, 2, 5, 4, 2, 1, 1, 0]),
         np.array([0, 0, 1, 3, 0, 1, 1]),
         np.array([1, 1, 3, 3, 4, 4, 2, 0]),
         np.array([0, 0, 1, 3, 2, 1, 0])
     ]
+
+    W = np.random.rand(5)
+    print(W)
+    print(sum(W))
     # medoid_ = Medoid(D)
     # print(medoid_)
     # print(DTW_multiple_alignment(medoid_, D[0]))
-    T = DBA(D, 5)
-    # print(T)
+    #T = DBA(D, 5)
+    # m = Medoid(D)
+    # w_m = Wtd_medoid(D, W)
+    # print(m)
+    # print(w_m)
+    Wtd_DBA(D, W, 5)
