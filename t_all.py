4d10e0ac15cab87687407873ddeb4502beb17b57
ics091 <icsrighttrack@outlook.com>
2020-08-07 20:25:43 +0800

    train loss caca modifi

--- t_all.py
+++ t_all.py
@@ -16,7 +16,6 @@ from matplotlib import pyplot as plt
 from data_loader import load_HNH
 from data_loader import load_HNH_siphon
 
-
 SEED = 1024
 torch.manual_seed(SEED)
 np.random.seed(SEED)
@@ -24,7 +23,9 @@ random.seed(SEED)
 
 
 def load_data():
-    time_list = ['2018-11', '2018-12', '2019-01', '2019-08', '2019-09', '2019-10']
+    time_list = [
+        '2018-11', '2018-12', '2019-01', '2019-08', '2019-09', '2019-10'
+    ]
     data_1811, label_1811 = load_HNH_siphon(52, 117, time_list[0])
     data_1812, label_1812 = load_HNH_siphon(40, 69, time_list[1])
     data_1901, label_1901 = load_HNH_siphon(10, 32, time_list[2])
@@ -32,10 +33,8 @@ def load_data():
     data_1909, label_1909 = load_HNH_siphon(25, 62, time_list[4])
     data_1910, label_1910 = load_HNH_siphon(45, 90, time_list[5])
 
-
     X = []
     Y = []
-
 
     for x, y in zip(data_1811, label_1811):
         X.append(x)
@@ -65,8 +64,11 @@ def load_data():
 
 
 def pre_data(X, y):
-    X_train, X_vail_test, y_train, y_vail_test = train_test_split(X, y, test_size=0.4)
-    X_vail, X_test, y_vail, y_test = train_test_split(X_vail_test, y_vail_test, test_size=0.5)
+    X_train, X_vail_test, y_train, y_vail_test = train_test_split(
+        X, y, test_size=0.4)
+    X_vail, X_test, y_vail, y_test = train_test_split(X_vail_test,
+                                                      y_vail_test,
+                                                      test_size=0.5)
     X_train = X_train.reshape(-1, 9)
     X_vail = X_vail.reshape(-1, 9)
     X_test = X_test.reshape(-1, 9)
@@ -112,12 +114,8 @@ class LSTM(nn.Module):
             num_layers=2,
             batch_first=True,
         )
-        self.dense = nn.Sequential(
-            nn.Dropout(0.5),
-            nn.Linear(16, 6),
-            nn.ReLU(),
-            nn.Linear(6, 1)
-        )
+        self.dense = nn.Sequential(nn.Dropout(0.5), nn.Linear(16, 6),
+                                   nn.ReLU(), nn.Linear(6, 1))
 
     def forward(self, x):
         x = x.permute(0, 2, 1)
@@ -137,7 +135,7 @@ LR = 0.001
 
 if __name__ == '__main__':
     X, y = load_data()
-    X = np.array(X)   # (1181, 30, 9)
+    X = np.array(X)  # (1181, 30, 9)
     y = np.array(y)
     print(X.shape)
     print(y.shape)
@@ -158,16 +156,21 @@ if __name__ == '__main__':
 
     lstm = LSTM()
     optimizer = torch.optim.Adam(lstm.parameters(), lr=LR)
-    loss_func = nn.BCEWithLogitsLoss() # 在BCELoss的基础上添加sigmoid
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=10, verbose=True)
+    loss_func = nn.BCEWithLogitsLoss()  # 在BCELoss的基础上添加sigmoid
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,
+                                                           'min',
+                                                           patience=10,
+                                                           verbose=True)
 
     epoch_rec = []
     train_loss_rec = []
-    loss_rec = []
+    test_loss_rec = []
     AUC_rec = []
     accu_rec = []
+    sum_loss = 0.0
+    sum_step = 1
 
-    for epoch in range(100):
+    for epoch in range(200):
         for step, (b_x, b_y) in enumerate(loader):
 
             b_x = b_x.view(-1, 30, 9)
@@ -175,13 +178,16 @@ if __name__ == '__main__':
             b_y = b_y.numpy().reshape(-1, 1)
             b_y = torch.from_numpy(b_y)
             loss = loss_func(output, b_y)
-            train_loss = loss
+            sum_loss = sum_loss + loss
+            sum_step = sum_step + 1
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            # print(loss.data.numpy())
 
             if step % 50 == 0:
+                train_loss_rec.append(sum_loss / sum_step)
+                sum_loss = 0.0
+                sum_step = 0
                 lstm = lstm.eval()
                 with torch.no_grad():
                     v_output = lstm(X_vail.view(-1, 30, 9))
@@ -190,16 +196,17 @@ if __name__ == '__main__':
                     v_loss = loss_func(v_output, y_vail)
                     true_vy = y_vail.numpy().reshape(-1, )
                     pred_vy = to_one_zero(torch.sigmoid(v_output).numpy())
-                    print('epoch', epoch + 1, '|step', step, '|loss: %.5f ' % v_loss.data.numpy(),
-                          '|AUC: %.5f ' % roc_auc_score(y_vail, torch.sigmoid(v_output)),
-                          '|accu: %.5f ' % accuracy_score(pred_vy, true_vy))
+                    print(
+                        'epoch', epoch + 1, '|step', step,
+                        '\n|loss: %.5f ' % v_loss.data.numpy(), '|AUC: %.5f ' %
+                        roc_auc_score(y_vail, torch.sigmoid(v_output)),
+                        '|accu: %.5f ' % accuracy_score(pred_vy, true_vy))
                     # print('pred_y:',pred_y)
                     # print('true_y:',true_y)
 
                     epoch_rec.append(epoch)
-                    train_loss_rec.append(train_loss.data.numpy())
-                    loss_rec.append(v_loss.data.numpy())
-                    AUC_rec.append(roc_auc_score(y_vail, torch.sigmoid(v_output)))
+                    AUC_rec.append(
+                        roc_auc_score(y_vail, torch.sigmoid(v_output)))
                     accu_rec.append(accuracy_score(pred_vy, true_vy))
                 lstm.train()
 
@@ -209,6 +216,7 @@ if __name__ == '__main__':
             y_test = y_test.numpy().reshape(-1, 1)
             y_test = torch.from_numpy(y_test)
             t_loss = loss_func(t_out, y_test)
+            test_loss_rec.append(t_loss)
             t_true_y = y_test.numpy().reshape(-1, )
             t_pred_y = to_one_zero(torch.sigmoid(t_out).numpy())
             print('|loss: %.5f ' % t_loss.data.numpy(),
@@ -216,17 +224,16 @@ if __name__ == '__main__':
                   '|accu: %.5f ' % accuracy_score(t_pred_y, t_true_y))
         lstm.train()
 
-
     epoch_rec = np.array(epoch_rec)
-    loss_rec = np.array(loss_rec)
     train_loss_rec = np.array(train_loss_rec)
+    test_loss_rec = np.array(test_loss_rec)
     AUC_rec = np.array(AUC_rec)
     accu_rec = np.array(accu_rec)
-    # plt.plot(epoch_rec,train_loss_rec,label='train-loss')
-    plt.plot(epoch_rec, loss_rec, label='log-loss')
-    plt.plot(epoch_rec, AUC_rec, label='AUC')
+    plt.plot(epoch_rec, train_loss_rec, label='train-loss')
+    plt.plot(epoch_rec, test_loss_rec, label='test-loss')
+    # plt.plot(epoch_rec, AUC_rec, label='AUC')
     # plt.plot(epoch_rec,accu_rec,label='accu')
     plt.xlabel('epoch')
-    plt.ylabel('logloss & AUC')
+    plt.ylabel('LOSS')
     plt.legend()
     plt.show()
